817db6cf88b48b85b97dee88ae1a9684aef667c3
markcalendario <markcalendario@gmail.com>
2023-07-10 23:28:45 +0800

    feat: add welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>hello</h1>;
+"use client";
+
+import Button from "@/components/Buttons/Buttons";
+import styles from "./page.module.scss";
+
+export default function Welcome() {
+  const handlePlayClick = () => {
+    window.location.href = "/play";
+  };
+
+  return (
+    <section id={styles.welcome}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <h1>Defuse Da Bomb</h1>
+          <Button onClick={handlePlayClick}>Play Now</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Buttons.module.scss";
+
+export default function Button(props) {
+  const { className, children, onClick } = props;
+
+  return (
+    <button className={styles.button + (className ? ` ${className}` : "")} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
