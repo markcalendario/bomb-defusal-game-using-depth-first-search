0673a16a98b7e14517137ae3120df4c93b1839de
markcalendario <markcalendario@gmail.com>
2023-07-13 15:27:35 +0800

    feat: working main game

--- src/app/play/page.js
+++ src/app/play/page.js
@@ -1,68 +1,155 @@
 "use client";
 import ToolButton from "@/components/ToolButton/ToolButton";
 import { defuseKit, generateDefuseKitTree, getAllToolsFromTree } from "@/functions/defuse-kit";
-import { useEffect, useState } from "react";
+import { Fragment, createContext, useContext, useEffect, useRef, useState } from "react";
 import styles from "./page.module.scss";
 
+const MainGameContext = createContext();
+const DefuseKitSequenceContext = createContext();
+
 export default function PlayCompiled() {
+  const gameAudio = useRef(new Audio("/assets/sounds/playing.mp3"));
   const [toolSequence, setToolSequence] = useState(null);
+  const [isSelectedCorrect, setIsSelectedCorrect] = useState(null);
   const [gameState, setGameState] = useState({
     isPlanted: false,
     isExploded: false,
     isDefused: false,
-    round: 1
+    level: 1,
+    timerLength: 15
   });
 
-  useEffect(() => {
+  const generateToolKit = () => {
     const rootTool = generateDefuseKitTree(5);
     setToolSequence(getAllToolsFromTree(rootTool));
-  }, []);
+  };
 
-  const setIsBombPlanted = () => {
+  const setIsDoneShowingToolsSequence = () => {
     setGameState((prev) => {
       return { ...prev, isPlanted: true };
     });
   };
 
+  const isSelectedToolCorrect = (isCorrect) => {
+    setIsSelectedCorrect(isCorrect);
+
+    setTimeout(() => {
+      setIsSelectedCorrect(null);
+    }, 500);
+  };
+
+  const setBombExploded = () => {
+    setGameState((prev) => {
+      return { ...prev, isExploded: true };
+    });
+  };
+
+  const setBombDefused = () => {
+    setGameState((prev) => {
+      return {
+        ...prev,
+        isDefused: true,
+        level: prev.level + 1,
+        timerLength: prev.timerLength - 1
+      };
+    });
+  };
+
+  const handleUserSelectTool = (toolID) => {
+    if (toolID === toolSequence[0].id) {
+      setToolSequence((prev) => {
+        prev.shift();
+        return [...prev];
+      });
+
+      return isSelectedToolCorrect(true);
+    }
+
+    isSelectedToolCorrect(false);
+
+    setTimeout(() => {
+      setBombExploded();
+    }, 500);
+  };
+
+  const handleNextLevel = () => {
+    generateDefuseKitTree(5 + gameState.level);
+    setGameState({ ...gameState, isPlanted: true, isExploded: false, isDefused: false });
+  };
+
+  useEffect(() => {
+    console.log(toolSequence);
+  }, [toolSequence]);
+
+  useEffect(() => {
+    if (toolSequence === null) return;
+    console.log(toolSequence.length);
+    if (toolSequence.length === 0) setBombDefused();
+  }, [toolSequence]);
+
+  useEffect(() => {
+    generateToolKit();
+  }, []);
+
   if (!gameState.isPlanted) {
-    return <ShowBombPlanting setIsBombPlanted={setIsBombPlanted} toolSequence={toolSequence} />;
+    return (
+      <DefuseKitSequenceContext.Provider
+        value={{
+          setIsDoneShowingToolsSequence,
+          toolSequence,
+          gameAudio
+        }}>
+        <ShowDefuseKitSequence />
+      </DefuseKitSequenceContext.Provider>
+    );
   }
 
-  if (gameState.isPlanted) {
-    return <MainGame />;
+  if (gameState.isPlanted && !gameState.isExploded && !gameState.isDefused) {
+    return (
+      <MainGameContext.Provider
+        value={{ gameState, handleUserSelectTool, isSelectedCorrect, setBombExploded }}>
+        <MainGame />
+      </MainGameContext.Provider>
+    );
+  }
+
+  if (gameState.isExploded) {
+    return <h1>Exploded.</h1>;
+  }
+
+  if (gameState.isDefused) {
+    return (
+      <Fragment>
+        <h1>Defused</h1>
+        <button onClick={handleNextLevel}>Next Level</button>
+      </Fragment>
+    );
   }
 }
 
-function ShowBombPlanting({ toolSequence, setIsBombPlanted }) {
+function ShowDefuseKitSequence() {
+  const { toolSequence, setIsDoneShowingToolsSequence, gameAudio } =
+    useContext(DefuseKitSequenceContext);
   const [currentDisplayedTool, setCurrentDisplayedTool] = useState(null);
-  const [isSequenceDone, setIsSequenceDone] = useState(false);
   const [isToolChanged, setIsToolChanged] = useState(0);
-
-  function playSound() {
-    new Audio("/assets/sounds/bomb-planted.wav").play();
-  }
-
-  function prepareGameStart() {
-    setTimeout(() => {
-      setIsBombPlanted();
-    }, 3000);
-  }
+  const [isSequenceDone, setIsSequenceDone] = useState(false);
 
   useEffect(() => {
-    // This effect is responsible for displaying a kit sequence.
     if (toolSequence === null) return;
 
     const toolSetQueue = [...toolSequence];
 
-    const toolInterval = setInterval(() => {
-      setCurrentDisplayedTool(toolSetQueue.shift()); // dequeue the first tool
-      setIsToolChanged((prev) => prev + 1); // responsible for animating sequence
-
+    const displayNextTool = () => {
       if (toolSetQueue.length === 0) {
         clearInterval(toolInterval);
         setIsSequenceDone(true);
       }
-    }, 2000);
+
+      setCurrentDisplayedTool(toolSetQueue.shift());
+      setIsToolChanged((prev) => prev + 1);
+    };
+
+    const toolInterval = setInterval(displayNextTool, 500);
 
     return () => {
       clearInterval(toolInterval);
@@ -71,10 +158,11 @@ function ShowBombPlanting({ toolSequence, setIsBombPlanted }) {
 
   useEffect(() => {
     if (isSequenceDone === false) return;
-
-    playSound();
-    prepareGameStart();
-  }, [isSequenceDone]);
+    gameAudio.current.play();
+    setTimeout(() => {
+      setIsDoneShowingToolsSequence();
+    }, 3000);
+  }, [isSequenceDone, gameAudio, setIsDoneShowingToolsSequence]);
 
   const displayTool = () => {
     return (
@@ -91,7 +179,7 @@ function ShowBombPlanting({ toolSequence, setIsBombPlanted }) {
   };
 
   return (
-    <section data-aos="fade-up" id={styles.sequenceDisplayer}>
+    <section id={styles.sequenceDisplayer}>
       <div className={styles.container}>
         <div className={styles.wrapper}>
           {isSequenceDone ? displayGuide("The bomb has been planted.") : null}
@@ -106,23 +194,79 @@ function ShowBombPlanting({ toolSequence, setIsBombPlanted }) {
 }
 
 function MainGame() {
+  const { gameState, handleUserSelectTool, isSelectedCorrect, setBombExploded } =
+    useContext(MainGameContext);
+  const [bombTime, setBombTime] = useState(null);
+
   const displayTools = () => {
     return defuseKit.map((tool, index) => (
-      <ToolButton key={index} imageLink={tool.image} toolName={tool.name} />
+      <ToolButton
+        key={index}
+        imageLink={tool.image}
+        toolName={tool.name}
+        onClick={() => {
+          handleUserSelectTool(tool.id);
+        }}
+      />
     ));
   };
 
+  // Initialize bomb timer
+  useEffect(() => {
+    setBombTime({ seconds: gameState.timerLength, milliseconds: 0 });
+  }, [gameState.timerLength]);
+
+  // Start timer countdown
+  useEffect(() => {
+    if (bombTime === null) return;
+
+    const timer = setInterval(() => {
+      if (bombTime.seconds === 0 && bombTime.milliseconds === 0) {
+        clearInterval(timer);
+        return;
+      }
+
+      setBombTime((prev) => {
+        if (prev.milliseconds === 0) {
+          return { seconds: bombTime.seconds - 1, milliseconds: 100 };
+        }
+
+        return { seconds: bombTime.seconds, milliseconds: bombTime.milliseconds - 1 };
+      });
+    }, 10);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [bombTime]);
+
+  useEffect(() => {
+    if (bombTime === null) return;
+
+    if (bombTime.seconds === 0 && bombTime.milliseconds === 0) {
+      setBombExploded();
+    }
+  }, [bombTime, setBombExploded]);
+
   return (
     <section id={styles.mainGame}>
-      <div className={styles.level}>Level 1</div>
+      <div className={styles.level}>Level {gameState.level}</div>
       <div className={styles.bombDisplay}>
-        <div className={styles.bomb}>
+        <div data-aos="fade-up" className={styles.bomb}>
           <div className={styles.timer}>
-            <h2>00:00:10</h2>
+            {bombTime !== null ? <h2>{`${bombTime.seconds}:${bombTime.milliseconds}`}</h2> : null}
             <p>Time Left</p>
           </div>
           <div className={styles.controls}>
-            <div className={styles.defuseScreen}></div>
+            <div
+              className={styles.defuseScreen}
+              style={{
+                backgroundColor: isSelectedCorrect
+                  ? "green"
+                  : isSelectedCorrect === false
+                  ? "red"
+                  : null
+              }}></div>
             <div className={styles.detonate}>
               <p>Detonate</p>
             </div>
